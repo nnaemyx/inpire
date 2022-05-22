eb11091abb86ae7b84ecf7ab4933b08c4370ebd7
johnpaul <edehjohnpaul@gmail.com>
2022-05-22 12:33:24 -0700

    card

--- src/components/PricingCards.js
+++ src/components/PricingCards.js
@@ -11,28 +11,28 @@ const PricingCards = () => {
                 <h3>- Basic -</h3>
                 <span className="bar"></span>
                 <p className="btc">$ 100</p>
-                <p>- 3 Days -</p>
-                <p>- 3 pages -</p>
+                <p>- 1 Day -</p>
+                <p>- 1 page -</p>
                 <p>- Featured</p>
                 <p>- Responsive Design</p>
                 <Link to="/contact" className=" btn">PURCHASE NOW</Link>
             </div>
 
             <div className="card">
-                <h3>- Basic -</h3>
+                <h3>- Premium -</h3>
                 <span className="bar"></span>
-                <p className="btc">$ 100</p>
-                <p>- 3 Days -</p>
-                <p>- 3 pages -</p>
+                <p className="btc">$ 200</p>
+                <p>- 2 Days -</p>
+                <p>- 2 pages -</p>
                 <p>- Featured</p>
                 <p>- Responsive Design</p>
                 <Link to="/contact" className="btn">PURCHASE NOW</Link>
             </div>
 
             <div className="card">
-                <h3>- Basic -</h3>
+                <h3>- Business -</h3>
                 <span className="bar"></span>
-                <p className="btc">$ 100</p>
+                <p className="btc">$ 300</p>
                 <p>- 3 Days -</p>
                 <p>- 3 pages -</p>
                 <p>- Featured</p>
