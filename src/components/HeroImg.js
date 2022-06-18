06bff8a4eebf6ba1e3bf3ead34f1faab23b5619b
johnpaul <edehjohnpaul@gmail.com>
2022-06-17 19:48:16 -0700

    new

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,9 +1,18 @@
-import "./HeroImgStyles.css";
-
 import React from 'react'
+import "./HeroImgStyles.css";
 
 import HeroImg from "../assests/hero-img.jpg"
 import { Link } from "react-router-dom";
+
+import FileSaver from 'file-saver';
+
+
+const saveFile = () => {
+  FileSaver.saveAs(
+    process.env.PUBLIC_URL + "/files/cv.pdf",
+    "MyCv.pdf"
+  );
+}
 
 const Heroimg = () => {
   return (
@@ -17,7 +26,9 @@ const Heroimg = () => {
             <div>
                 <Link to="/project" className="btn">Projects</Link>
                 <Link to="/contact" className=" btn btn-light">Contact</Link>
-                <Link to="/contact" className=" btn btn-light">Download CV</Link>
+                <button className="btn btn-light" onClick={saveFile}>
+                  Download Resume
+                </button>
             </div>
         </div>
 
