c2bb043479ab631dac9c68551e73df8d0428ace5
tezon <tezonteam@gmail.com>
2022-08-16 02:49:01 -0700

    Added some new projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,61 +1,77 @@
 import "./FooterStyles.css"
 
 import React from 'react'
-import {FaFacebook, FaInstagram, FaMailBulk, FaPhone, FaWhatsapp} from "react-icons/fa"
+import { FaFacebook, FaMailBulk, FaPhone, FaWhatsapp } from "react-icons/fa"
+import {AiFillTwitterSquare} from "react-icons/ai"
 
 const Footer = () => {
-    return  (
-        <div className="footer">
-            <div className="footer-container">
-                <div className="left">
-                
-                    
-                    <div className="phone">
-                        <h4><FaPhone size={20} style={{color: 
-                        "white", marginRight:"2rem"}} />
-                        +234 8098240363  </h4>
-                    
-                    </div>
-                    <div className="email">
-                        <h4><FaMailBulk size={20} style={{color: 
-                        "white", marginRight:"2rem"}} />
-                        edehjohnpaul@gmail.com</h4>
-                    
-                    </div>
-                </div>
-                <div className="right">
-                    <h4>About the company</h4>
-                    <p>I'M edeh johnpaul chukwuemeka, CEO
-                        Founder of MernDock. I enjoy discussing 
-                        new projects and design Challenges.
-                    </p>
-                    <div className="social">
-                        <a href="https://www.facebook.com/edeh.johnpaul.7982/" target="_blank" rel="noreferrer" >
-                           <FaFacebook size={30} style={{color: 
-                                "white", marginRight:"2rem"}} 
-                            />
-                        </a>
-                        
-
-                        <a href="https://www.instagram.com/merndev61/" target="_blank" rel="noreferrer" >
-                           <FaInstagram size={30} style={{color: 
-                                "white", marginRight:"2rem"}} 
-                            />
-                        </a>
-
-                        <a href="https://wa.me/message/ZHSHNKGMSANAG1/" target="_blank"  rel="noreferrer">
-                           <FaWhatsapp size={30} style={{color: 
-                                "white", marginRight:"2rem"}} 
-                            />
-                        </a>
-                           
-                    </div>   
-                    
-                </div>
-
+    return (
+      <div className="footer">
+        <div className="footer-container">
+          <div className="left">
+            <div className="phone">
+              <h4>
+                <FaPhone
+                  size={20}
+                  style={{ color: "white", marginRight: "2rem" }}
+                />
+                +234 8098240363{" "}
+              </h4>
             </div>
+            <div className="email">
+              <h4>
+                <FaMailBulk
+                  size={20}
+                  style={{ color: "white", marginRight: "2rem" }}
+                />
+                edehjohnpaul@gmail.com
+              </h4>
+            </div>
+          </div>
+          <div className="right">
+            <h4>About the company</h4>
+            <p>
+              I'M edeh johnpaul chukwuemeka, CEO Founder of MernDock. I enjoy
+              discussing new projects and design Challenges.
+            </p>
+            <div className="social">
+              <a
+                href="https://www.facebook.com/profile.php?id=100083084891109"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebook
+                  size={30}
+                  style={{ color: "white", marginRight: "2rem" }}
+                />
+              </a>
+
+              <a
+                href="https://twitter.com/EdehJohnpaul"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <AiFillTwitterSquare
+                  size={30}
+                  style={{ color: "white", marginRight: "2rem" }}
+                />
+              </a>
+
+              <a
+                href="https://wa.me/2348098240363"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaWhatsapp
+                  size={30}
+                  style={{ color: "white", marginRight: "2rem" }}
+                />
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
         
 }
 
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,55 +1,78 @@
-import pro1 from "../assests/project1.jpg"
-import pro2 from "../assests/picture2.jpg"
-import pro3 from "../assests/picture3.jpg"
-import pro4 from "../assests/picture5.jpg"
-import pro5 from "../assests/picture6.jpg"
-import pro6 from "../assests/picture4.png"
+import pro1 from "../assests/project1.jpg";
+import pro2 from "../assests/picture2.jpg";
+import pro3 from "../assests/picture3.jpg";
+import pro4 from "../assests/picture5.jpg";
+import pro5 from "../assests/picture6.jpg";
+import pro6 from "../assests/picture4.png";
+import pro7 from "../assests/picture7.jpg";
+import pro8 from "../assests/picture8.jpg";
+import pro9 from "../assests/picture9.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: pro1,
-        title: "E-commerce Website Design",
-        text: "Responsive e-commerce website for a fashion store. STILL UNDER PRODUCTION",
-        view:"/",
-        source:"/"
-    },
-    {
-        imgsrc: pro2,
-        title: "E-commerce Website Design",
-        text: "Responsive E-commerce website for selling motivational Videos and E-books",
-        view:"https://www.eemademylifeproduction.com",
-        source:""
-    },
+  {
+    imgsrc: pro1,
+    title: "E-commerce Website Design",
+    text: "Responsive e-commerce website for a fashion store. STILL UNDER PRODUCTION",
+    view: "/",
+    source: "/",
+  },
+  {
+    imgsrc: pro2,
+    title: "E-commerce Website Design",
+    text: "Responsive E-commerce website for selling motivational Videos and E-books",
+    view: "https://www.eemademylifeproduction.com",
+    source: "",
+  },
 
-    {
-        imgsrc: pro3,
-        title: "Restuarant Website Design",
-        text: "Thriving responsive restuarant design",
-        view:"https://shrg-roup46-capstone-project.vercel.app/",
-        source:"https://github.com/nnaemyx/-SHRGroup46_Capstone-project"
-    },
-    {
-        imgsrc: pro4,
-        title: "Adventure Website Design",
-        text: "Responsive website for Adventure lovers,",
-        view:"https://adventure2.herokuapp.com/",
-        source:"https://github.com/nnaemyx/adventure"
-    },
-    {
-        imgsrc: pro5,
-        title: "Portfolio Website Design",
-        text: "Thriving responsive Portfolio design",
-        view:"https://inpire.vercel.app/",
-        source:"https://github.com/nnaemyx/inpire"
-    },
-    {
-        imgsrc: pro6,
-        title: "Data Analytics Website Design",
-        text: "Thriving responsive Portfolio design, developed with react and tailwindcss",
-        view:"https://tailwind-react-nnaemyx.vercel.app/",
-        source:"https://github.com/nnaemyx/tailwind-react"
-    },
-  
-]
+  {
+    imgsrc: pro3,
+    title: "Restuarant Website Design",
+    text: "Thriving responsive restuarant design",
+    view: "https://shrg-roup46-capstone-project.vercel.app/",
+    source: "https://github.com/nnaemyx/-SHRGroup46_Capstone-project",
+  },
+  {
+    imgsrc: pro4,
+    title: "Adventure Website Design",
+    text: "Responsive website for Adventure lovers,",
+    view: "https://adventure2.herokuapp.com/",
+    source: "https://github.com/nnaemyx/adventure",
+  },
+  {
+    imgsrc: pro5,
+    title: "Portfolio Website Design",
+    text: "Thriving responsive Portfolio design",
+    view: "https://inpire.vercel.app/",
+    source: "https://github.com/nnaemyx/inpire",
+  },
+  {
+    imgsrc: pro6,
+    title: "Data Analytics Website Design",
+    text: "Thriving responsive Portfolio design, developed with react and tailwindcss",
+    view: "https://tailwind-react-nnaemyx.vercel.app/",
+    source: "https://github.com/nnaemyx/tailwind-react",
+  },
+  {
+    imgsrc: pro7,
+    title: "Data Analytics Website Design",
+    text: "A Goalsetter site built with MERN stack and also Redux",
+    view: "https://mernappjayp.herokuapp.com/",
+    source: "https://github.com/nnaemyx/MERN-tutorial",
+  },
+  {
+    imgsrc: pro8,
+    title: "Workout Website",
+    text: "In this site i made use of the MERN stack",
+    view: "https://mern-tutjayp.herokuapp.com/",
+    source: "https://github.com/nnaemyx/MERN-STACK",
+  },
+  {
+    imgsrc: pro9,
+    title: "FINTECH Website design",
+    text: "Thriving responsive design, developed with react and tailwindcss. Design gotten from twitter",
+    view: "https://savespace-lime.vercel.app/",
+    source: "https://github.com/nnaemyx/savespace",
+  },
+];
 
-export default ProjectCardData
+export default ProjectCardData;
